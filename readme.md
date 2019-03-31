b4935f67d0ac09f9b16224c7b7045184b5316ce4
Michal Michalski <buyuk.dev@gmail.com>
2019-03-31 12:25:56 +0200

    Init repo + code framework.

---
+++ readme.md
@@ -0,0 +1,7 @@
+1. Build project:
+
+    g++ -o bin/seq src/main.cpp -std=c++17
+
+2. Run project:
+
+    ./bin/seq
---
+++ src/main.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <set>
+
+
+using namespace std;
+
+
+struct Config
+{
+    Config() :
+        k(3), 
+        dna("ACGTATAAAGGCTAGTTTATTACCCCGGGT"),
+        n(dna.size())
+    {
+    }
+
+    const string dna;
+    const int k;
+    const int n;
+};
+
+
+set<string> buildCompleteSpectrum(const Config& config)
+{
+    set<string> spectrum;
+    for (int pos=0; pos<config.n - config.k; ++pos) {
+        spectrum.insert(config.dna.substr(pos, config.k));
+    }
+    return spectrum;
+}
+
+
+string reconstructDna(const set<string>& spectrum, const Config& config)
+{
+    string dna;
+    return dna;
+}
+
+
+double compareDna(const string& dna1, string& dna2)
+{
+    if (dna1 == dna2) {
+        return 1.0;
+    }
+    return 0.0;
+}
+
+
+int main(int argc, char* argv[])
+{
+    Config config;
+
+    auto spectrum = buildCompleteSpectrum(config);
+    cout << "SPECTRUM -> { ";
+    for (const auto& oligo: spectrum) {
+        cout << oligo << " ";
+    }
+    cout << "}\n\n";
+
+    string dna = reconstructDna(spectrum, config);
+    cout << "RECONSTRUCTED DNA:\n";
+    cout << dna << endl << endl;
+
+    double similarity = compareDna(config.dna, dna);
+    cout << "SIMILARITY LEVEL: " << similarity << endl;
+    return 0;
+}
