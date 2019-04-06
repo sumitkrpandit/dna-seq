c6eace79090fde1faba6e64df56232afafaee576
Michal Michalski <buyuk.dev@gmail.com>
2019-04-06 19:16:04 +0200

    something something.

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <vector>
 #include <string>
-#include <set>
 #include <algorithm>
 
 #include "levensthein.hpp"
@@ -13,33 +12,37 @@
 using namespace std;
 
 
-string reconstructDna(const set<string>& spectrum, const string& startOligo, const Config& config)
+vector<vector<int>> buildGraph(const vector<string>& spectrum)
 {
-    vector<string> nodes;
-    copy(spectrum.begin(), spectrum.end(), back_inserter(nodes));
-
-    auto _start = find(nodes.begin(), nodes.end(), startOligo);
-    int id = distance(nodes.begin(), _start);
-
-    // compute distances
-    vector<vector<int>> distances(nodes.size(), vector<int>(nodes.size(), 0));
-    for (int i=0; i<nodes.size(); ++i) {
+    vector<vector<int>> distances(spectrum.size(), vector<int>(spectrum.size(), 0));
+    for (int i=0; i<spectrum.size(); ++i) {
         distances[i][i] = -1;
-        for (int j=0; j<nodes.size(); ++j) {
+        for (int j=0; j<spectrum.size(); ++j) {
             if (i == j) continue;
-            distances[i][j] = overlap(nodes[i], nodes[j]);
-            distances[j][i] = overlap(nodes[j], nodes[i]); 
+            distances[i][j] = overlap(spectrum[i], spectrum[j]);
+            distances[j][i] = overlap(spectrum[j], spectrum[i]); 
         }
     }
+    return distances;
+}
 
-    // reconstruction
-    string dna = nodes[id];
+
+string reconstructDna(const vector<string>& spectrum, const string& first, const Config& config)
+{
+    auto distances = buildGraph(spectrum);
+
+    int id = distance(
+        spectrum.begin(), 
+        find(spectrum.begin(), spectrum.end(), first)
+    );
+
+    string dna = spectrum[id];
     while (dna.size() < config.n) {
         int bestId = findMax(distances[id]);
-        dna += nodes[bestId][config.k-1];
+        dna += spectrum[bestId][config.k-1];
         id = bestId;
     }
- 
+
     return dna;
 }
 
@@ -47,8 +50,8 @@ string reconstructDna(const set<string>& spectrum, const string& startOligo, con
 int main(int argc, char* argv[])
 {
     auto config = readConfigFromUser();
-
     auto spectrum = buildCompleteSpectrum(config);
+   
     cout << "SPECTRUM -> { ";
     for (const auto& oligo: spectrum) {
         cout << oligo << " ";
@@ -56,11 +59,11 @@ int main(int argc, char* argv[])
     cout << "}\n\n";
 
     string dna = reconstructDna(spectrum, config.dna.substr(0, config.k), config);
-    cout << "RECONSTRUCTED DNA [" << dna.size() << "]\n";
-    cout << dna << endl << endl;
+    cout << "RECONSTRUCTED DNA\n";
+    cout << dna << "\n\n";
 
     cout << "ORIGINAL DNA [" << config.n << "]\n";
-    cout << config.dna << endl << endl;
+    cout << config.dna << "\n\n";
 
     auto similarity = compareDna(config.dna, dna);
     cout << "SIMILARITY LEVEL: " << similarity << "%\n";
--- src/simulation.hpp
+++ src/simulation.hpp
@@ -2,27 +2,27 @@
 #define SIMULATION_HPP
 
 
-#include <set>
+#include <vector>
 #include <string>
 #include "config.hpp"
 
 
-std::set<std::string> buildCompleteSpectrum(const Config& config)
+std::vector<std::string> buildCompleteSpectrum(const Config& config)
 {
-    std::set<std::string> spectrum;
+    std::vector<std::string> spectrum;
     for (int pos=0; pos<config.n - config.k + 1; ++pos) {
-        spectrum.insert(config.dna.substr(pos, config.k));
+        spectrum.push_back(config.dna.substr(pos, config.k));
     }
     return spectrum;
 }
 
 
-void injectPositiveError(std::set<std::string>& spectrum)
+void injectPositiveError(std::vector<std::string>& spectrum)
 {
 }
 
 
-void injectNegativeError(std::set<std::string>& spectrum)
+void injectNegativeError(std::vector<std::string>& spectrum)
 {
 }
 
